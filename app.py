36e6c7a9852eb3f586978de42a0c8a28588eff65
KANUMURI HARSHITH <111886682+harshith1315@users.noreply.github.com>
2023-08-10 22:34:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import openai
 import streamlit as st
 
-st.title("CHATGPT INTEGRATED CHATBOT",anchor="str")
 st.set_page_config(page_title="KH-CHATBOT", page_icon = 'screenshot (2).png')
+st.title("CHATGPT INTEGRATED CHATBOT",anchor="str")
+
 openai.api_key = st.secrets["key"]
 try:
     if "openai_model" not in st.session_state:
