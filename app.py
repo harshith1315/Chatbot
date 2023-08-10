25dfedb78ceb35e4282bfb3b3010619be02dd0b9
KANUMURI HARSHITH <111886682+harshith1315@users.noreply.github.com>
2023-08-10 18:05:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ try:
             stop = None,
             temperature=0.5,
         )
-        message = completions.choices[0]
+        message = completions.choices[0].delta
         return message 
 
 
