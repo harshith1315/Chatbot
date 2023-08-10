16d0ab4dbb993b3ab6bbeaab0bb7693f0bfc9d8e
KANUMURI HARSHITH <111886682+harshith1315@users.noreply.github.com>
2023-08-10 23:36:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ openai.api_key = st.secrets["key"]
 st.title("CHATGPT INTEGRATED CHATBOT", anchor="str")
 
 if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "text-davinci-003"
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
